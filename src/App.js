15d1a6f165a535b24c867fe34da90bbbc2bf0d15
muhammadasif84 <mohammedasif80902@gmail.com>
2021-02-03 17:28:44 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
   return (
     <div className="App">
       <Dinner dishName= "Chicken Biryani"/>
+      <Dinner dishName= "Chicken Karahi"/>   
+      <Dinner dishName= "Gajar ka Halwa"/>
     </div>
   );
 }
